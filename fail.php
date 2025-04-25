192687fce85c134ec5ad2ec8b2640ee64c323f77
mutsinzisamuel <mutsinzisamuel5@gmail.com>
2025-04-25 19:22:12 +0300

    Add files via upload

---
+++ fail.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>NIYITEGEKA HASSAN</title>
+</head>
+<body>
+<?php
+    print hash('sha256', 'NIYITEGEKA HASSAN')
+?>
+</body>
+</html>
