1851f773bd4d8f54e9a7ecb3cd5b46e1ee7c7f0e
mutsinzisamuel <mutsinzisamuel5@gmail.com>
2025-04-25 19:21:06 +0300

    Add files via upload

---
+++ check.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Niyitegeka Hassan PHP</title>
+</head>
+<body>
+    <h1>Niyitegeka Hassan PHP</h1>
+
+    <pre>
+    N   N
+    N   N
+    N N N
+    N   N
+    N   N
+    </pre>
+
+    <?php
+        echo "<p>SHA256 of your name is: " . hash('sha256', 'Niyitegeka Hassan') . "</p>";
+    ?>
+</body>
+</html>
